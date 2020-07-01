92a44072a73835ac69f7fee16fddf606df978382
amareshsm <amareshsm13@gmail.com>
2020-07-01 22:55:54 +0530

    React PWA

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import {FetchWeather} from './Api/FetchWeather'
 const App=()=> {
 
    const [query,setQuery] = useState('');
+   const [weather, setWeather] = useState('')
 
    const search = async (e) =>{
      if(e.key ==='Enter'){
        const data = await FetchWeather(query);
-       console.log(data);
+      // console.log(data);
+       setWeather(data)
+       setQuery('');
      }
    }
 
@@ -21,6 +24,25 @@ const App=()=> {
              onChange={(e) => setQuery(e.target.value)}          
              onKeyPress={search}  
              />
+
+            {weather.main && (
+                <div className="city">
+                  <h2 className="city-name">
+                       <span>{weather.name}</span>
+                       <sup>{weather.sys.country}</sup>
+                  </h2>
+                     <div className="city-temp">
+                       {Math.round(weather.main.temp)}
+                       <sup>&deg; C</sup>
+                       </div>
+                       <div className="info">
+                       <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+                        <p>{weather.weather[0].description}</p>
+                       </div>
+                  </div>
+
+            )}
+             
 
 
          </div>
